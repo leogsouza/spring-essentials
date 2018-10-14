2f0ea1c38c1842715f5de316924aed0a287eeddc
Leonardo Souza <leogsouza@gmail.com>
2018-10-14 12:09:41 -0300

    Using Autowired component and SpringBootApplication annotations

--- src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
@@ -1,11 +1,15 @@
 package br.com.leogsouza.awesome.endpoint;
 
 import br.com.leogsouza.awesome.model.Student;
+import br.com.leogsouza.awesome.util.DateUtil;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -13,9 +17,12 @@ import static java.util.Arrays.asList;
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
+    @Autowired
+    private DateUtil dateUtil;
 
     @RequestMapping(method = RequestMethod.GET, path = "/list")
     public List<Student> listAll() {
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return asList(new Student("Leonardo"), new Student("Fabiana"));
     }
 }

527b9e89e1d37e2e0f11ff63aa7f7f5b67098d4a
Leonardo Souza <leogsouza@gmail.com>
2018-10-14 19:11:03 -0300

    Students get requests

--- src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
@@ -1,28 +1,48 @@
 package br.com.leogsouza.awesome.endpoint;
 
+import br.com.leogsouza.awesome.error.CustomErrorType;
 import br.com.leogsouza.awesome.model.Student;
 import br.com.leogsouza.awesome.util.DateUtil;
-import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
-    @Autowired
-    private DateUtil dateUtil;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll() {
-        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return asList(new Student("Leonardo"), new Student("Fabiana"));
+    private final DateUtil dateUtil;
+
+    @Autowired
+    public StudentEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listAll() {
+        // System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path= "/{id}")
+    public ResponseEntity<?> listStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+
+        if (index == -1) {
+            return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 }

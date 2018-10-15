8afec89af9a097f779a3f6f041d1bedde940e75f
Leonardo Souza <leogsouza@gmail.com>
2018-10-14 22:14:03 -0300

    Save students into a list

--- src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/leogsouza/awesome/endpoint/StudentEndpoint.java
@@ -6,10 +6,7 @@ import br.com.leogsouza.awesome.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -44,5 +41,12 @@ public class StudentEndpoint {
         }
 
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+
+        return new ResponseEntity<Object>(student, HttpStatus.OK);
     }
 }

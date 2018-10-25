1d489806c96fe798cc85b95e48a0222fa98065da
Leonardo Souza <leonardo.goncalves@gft.com>
2018-10-25 07:38:41 -0300

    Testing API requests with pure Java

---
+++ src/main/java/br/com/leogsouza/awesome/javaclient/JavaClientTest.java
@@ -0,0 +1,46 @@
+package br.com.leogsouza.awesome.javaclient;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import sun.nio.ch.IOUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class JavaClientTest {
+
+    public static void main(String[] args) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        String username = "mjiruca1";
+        String password = "1234";
+        try {
+            URL url = new URL("http://localhost:8080/students/3");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.addRequestProperty("Authorization", "Basic "+ encodeUsernamePassword(username, password));
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder jsonSB = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonSB.append(line);
+            }
+            System.out.println(jsonSB.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(reader);
+            if (connection != null)
+                connection.disconnect();
+        }
+    }
+
+    private static String encodeUsernamePassword(String username, String password) {
+        String userPassword = username + ":"+ password;
+
+        return new String(Base64.encodeBase64(userPassword.getBytes()));
+    }
+}

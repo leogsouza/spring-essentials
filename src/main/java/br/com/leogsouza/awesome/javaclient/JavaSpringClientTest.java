7a78779a35669677e5ab192831fff3417f76c076
Leonardo Souza <leogsouza@gmail.com>
2018-11-04 14:39:20 -0200

    Add RestTemplate for post method

--- src/main/java/br/com/leogsouza/awesome/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/leogsouza/awesome/javaclient/JavaSpringClientTest.java
@@ -4,8 +4,7 @@ import br.com.leogsouza.awesome.model.PageableResponse;
 import br.com.leogsouza.awesome.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -19,11 +18,22 @@ public class JavaSpringClientTest {
                 .basicAuthorization("mjiruca1", "1234").build();
         Student student = restTemplate.getForObject("/{id}", Student.class, 1);
         System.out.println(student);
-        Student[] students = restTemplate.getForObject("/", Student[].class);
-        System.out.println(Arrays.toString(students));
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-                new ParameterizedTypeReference<PageableResponse<Student>>() {
-                });
-        System.out.println(exchange);
+//        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+//                new ParameterizedTypeReference<PageableResponse<Student>>() {
+//                });
+//        System.out.println(exchange);
+
+        Student studentPost = new Student();
+        studentPost.setName("Ash Dolphin");
+        studentPost.setEmail("ashdp@asp.edu");
+        ResponseEntity<Student> exchangePost = restTemplate.exchange("/",
+                HttpMethod.POST, new HttpEntity<>(studentPost, createJSONHeader()), Student.class);
+        System.out.println(exchangePost);
+    }
+
+    private static HttpHeaders createJSONHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
     }
 }

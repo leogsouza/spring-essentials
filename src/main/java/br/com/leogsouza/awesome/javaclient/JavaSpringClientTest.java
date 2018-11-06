cedf7a150e3b2b1b400d2ff33a3cd0d437bfd0ca
Leonardo Souza <leonardo.goncalves@gft.com>
2018-11-06 10:01:40 -0200

    Fix pagination with rest template

--- src/main/java/br/com/leogsouza/awesome/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/leogsouza/awesome/javaclient/JavaSpringClientTest.java
@@ -1,14 +1,12 @@
 package br.com.leogsouza.awesome.javaclient;
 
+import br.com.leogsouza.awesome.model.PageableResponse;
 import br.com.leogsouza.awesome.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class JavaSpringClientTest {
 
@@ -18,11 +16,15 @@ public class JavaSpringClientTest {
                 .basicAuthorization("mjiruca1", "1234").build();
         Student student = restTemplate.getForObject("/{id}", Student.class, 1);
         System.out.println(student);
-        Student[] students = restTemplate.getForObject("/", Student[].class);
-        System.out.println(Arrays.toString(students));
-        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Student>>() {
+        // Student[] students = restTemplate.getForObject("/", Student[].class);
+        // System.out.println(Arrays.toString(students));
+        // ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+        //        new ParameterizedTypeReference<List<Student>>() {
+        //        });
+
+        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageableResponse<Student>>() {
                 });
-        System.out.println(exchange.getBody());
+        System.out.println(exchange);
     }
 }
--- src/main/java/br/com/leogsouza/awesome/model/PageableResponse.java
+++ src/main/java/br/com/leogsouza/awesome/model/PageableResponse.java
@@ -1,24 +1,75 @@
 package br.com.leogsouza.awesome.model;
 
+import br.com.leogsouza.awesome.util.CustomSortDeserializer;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PageableResponse<T> extends PageImpl<T> {
 
-    public PageableResponse(@JsonProperty("content")List<T> content,
+    private boolean last;
+    private boolean first;
+    private int totalPages;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public PageableResponse(@JsonProperty("content") List<T> content,
                             @JsonProperty("number") int page,
                             @JsonProperty("size") int size,
-                            @JsonProperty("totalElements") Long totalElements
+                            @JsonProperty("totalElements") Long totalElements,
+                            @JsonProperty("pageable") JsonNode pageable,
+                            @JsonProperty("last") boolean last,
+                            @JsonProperty("totalPages") int totalPages,
+                            @JsonProperty("sort") @JsonDeserialize(using = CustomSortDeserializer.class) Sort sort,
+                            @JsonProperty("first") boolean first,
+                            @JsonProperty("numberOfElements") int numberOfElements
                             ) {
-        super(content, PageRequest.of(page, size), totalElements);
+        super(content, PageRequest.of(page, size, sort), totalElements);
+    }
+
+    public PageableResponse(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public PageableResponse(List<T> content) {
+        super(content);
     }
 
     public PageableResponse() {
         super(new ArrayList<>());
+    }
+
+    @Override
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+
+    @Override
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    @Override
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
     }
 }
